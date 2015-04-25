1e73054e552b9d267c3ebc73d1a8cd92b3128430
Marko Raatikainen <marko.m.raatikainen@gmail.com>
2015-04-25 17:08:29 +0300

    Only run if executed as main
    This results in better cooperation with Emacs robe-mode.

--- lib/choose_pictures.rb
+++ lib/choose_pictures.rb
@@ -3,6 +3,8 @@ require_relative 'copy_pictures'
 require_relative 'current_picture'
 require_relative 'selection'
 
-QML.run do |app|
-  app.load_path Pathname(__FILE__) + '../main.qml'
+if __FILE__ == $PROGRAM_NAME
+  QML.run do |app|
+    app.load_path Pathname(__FILE__) + '../main.qml'
+  end
 end
